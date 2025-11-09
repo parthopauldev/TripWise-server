8c66a9ba5f52c27c59affa5ae12bf22914531a78
parthopauldev <parthopaul.dev@gmail.com>
2025-11-09 06:23:53 -0600

    mongodb has been connected

--- index.js
+++ index.js
@@ -1,15 +1,41 @@
 const express = require('express')
 const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const app = express()
 const port = process.env.PORT || 3000
 
 // middleware
 app.use(cors());
 app.use(express.json())
+// TripWiseUser
+// FeIbvw0xK5xQXEf1
+const uri = "mongodb+srv://TripWiseUser:FeIbvw0xK5xQXEf1@simpleapp.suaq6bu.mongodb.net/?appName=simpleapp";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
 
 app.get('/', (req, res) => {
   res.send('Tripwise server is running...')
 })
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+   
+      
+  }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
   console.log(`Tripwise server is running on port: ${port}`)
