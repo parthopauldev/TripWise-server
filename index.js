b58ab5623bcce7f50551ea7c2ea32247cfba84c1
parthopauldev <parthopaul.dev@gmail.com>
2025-11-11 02:12:23 -0600

    Created product get api for get user created product

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-const express = require('express')
-const cors = require('cors')
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
-const app = express()
-const port = process.env.PORT || 3000
+const app = express();
+const port = process.env.PORT || 3000;
 
 // middleware
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 // TripWiseUser
 // FeIbvw0xK5xQXEf1
-const uri = "mongodb+srv://TripWiseUser:FeIbvw0xK5xQXEf1@simpleapp.suaq6bu.mongodb.net/?appName=simpleapp";
+const uri =
+  "mongodb+srv://TripWiseUser:FeIbvw0xK5xQXEf1@simpleapp.suaq6bu.mongodb.net/?appName=simpleapp";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -18,78 +19,87 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
-app.get('/', (req, res) => {
-  res.send('Tripwise server is running...')
-})
+app.get("/", (req, res) => {
+  res.send("Tripwise server is running...");
+});
 
 async function run() {
   try {
     await client.connect();
     const myDB = client.db("TripWiseUser");
-    
-   const productsCollection= myDB.collection("products");
-   const bookProductCollection= myDB.collection("bookProducts");
-   
+
+    const productsCollection = myDB.collection("products");
+    const bookProductCollection = myDB.collection("bookProducts");
+
     // products get api for get all product
-    app.get('/products', async (req, res) => {
-      const cursor = productsCollection.find()
+    app.get("/products", async (req, res) => {
+      const cursor = productsCollection.find();
       const result = await cursor.toArray();
       res.send(result);
-    })
+    });
     // products get api for get single product
-    app.get('/products/:id', async (req, res) => {
+    app.get("/products/:id", async (req, res) => {
       const id = req.params.id;
-      const query = { _id: new ObjectId(id) }
+      const query = { _id: new ObjectId(id) };
       const result = await productsCollection.findOne(query);
       res.send(result);
-    })
+    });
     // products Post api
-    app.post('/products', async(req,res) => {
+    app.post("/products", async (req, res) => {
       const newProduct = req.body;
       const result = await productsCollection.insertOne(newProduct);
-      res.send(result)
-    })
+      res.send(result);
+    });
     // products patch api
-    app.patch('/products/:id', async (req, res) => {
+    app.patch("/products/:id", async (req, res) => {
       const id = req.params.id;
       const updateProduct = req.body;
       const query = { _id: new ObjectId(id) };
       const update = {
         $set: {
-          vehicleName: updateProduct.vehicleName
-           
-        }
-      }
+          vehicleName: updateProduct.vehicleName,
+        },
+      };
       const result = await productsCollection.updateOne(query, update);
-      res.send(result)
-
-    })
+      res.send(result);
+    });
     // products Delete api
-    app.delete('/products/:id',async(req, res) => {
+    app.delete("/products/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await productsCollection.deleteOne(query);
-      res.send(result)
-    })
-    // bookProducts post api 
-app.post('/bookProducts', async (req, res) => {
-            const newBid = req.body;
-            const result = await bookProductCollection.insertOne(newBid);
-            res.send(result);
-        })
+      res.send(result);
+    });
+    // bookProducts post api
+    app.post("/bookProducts", async (req, res) => {
+      const newBid = req.body;
+      const result = await bookProductCollection.insertOne(newBid);
+      res.send(result);
+    });
+    app.get("/products", async (req, res) => {
+      const email = req.query.email;
+      const query = {};
+      if (email) {
+        query.userEmail = email;
+      }
+
+      const cursor = productsCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
-   
-      
   }
 }
 run().catch(console.dir);
 
 app.listen(port, () => {
-  console.log(`Tripwise server is running on port: ${port}`)
-})
+  console.log(`Tripwise server is running on port: ${port}`);
+});
